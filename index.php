619a1e997684453a70f17b25c7c6bdf8201238b3
Kaankst <139494018+Kaankst@users.noreply.github.com>
2025-03-21 19:00:45 +0200

    updateing the title and adding h1 tag

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Chuck Norris</title>
 </head>
 <body>
+<h1>😂 Chuck Norris Joke Generator</h1>
+
     
 </body>
 </html>
